4db4c81a5410e5e695c55037743e656139406b36
TheReeds <tomas159357852@gmail.com>
2024-11-22 14:37:28 -0500

    da

--- src/app/document/document.component.ts
+++ src/app/document/document.component.ts
@@ -1,6 +1,7 @@
 // word-document.service.ts
 import {Component} from '@angular/core';
 import {DocumentService} from './word-documen.service';
+import {DocumentService2} from './word-documen2.service';
 
 @Component({
   selector: 'app-document',
@@ -9,10 +10,13 @@ import {DocumentService} from './word-documen.service';
   styleUrl: './document.component.scss'
 })
 export class DocumentComponent {
-  constructor(private documentService: DocumentService) {
+  constructor(private documentService: DocumentService, private documentService2: DocumentService2 ) {
   }
 
   downloadDocument(): void {
     this.documentService.generateDocument();
+  }
+  downloadDocument2(): void {
+    this.documentService2.generateDocument();
   }
 }
